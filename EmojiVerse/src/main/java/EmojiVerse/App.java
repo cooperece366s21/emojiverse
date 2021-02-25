fa95a9144e86ca072ae24ff709a67479685ea73b
Nikita Teplitskiy <nteplitskiy@localhost.localdomain>
2021-02-24 20:10:43 -0500

    add maven project, basic hello world

---
+++ EmojiVerse/src/main/java/EmojiVerse/App.java
@@ -0,0 +1,13 @@
+package EmojiVerse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
