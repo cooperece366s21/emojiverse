be9c3527c5175452bf388b4fdcbda56162067e98
Nikita Teplitskiy <nteplitskiy@localhost.localdomain>
2021-02-24 20:43:39 -0500

    spark hello world, move to java 11

--- EmojiVerse/src/main/java/EmojiVerse/App.java
+++ EmojiVerse/src/main/java/EmojiVerse/App.java
@@ -1,4 +1,5 @@
 package EmojiVerse;
+import static spark.Spark.*;
 
 /**
  * Hello world!
@@ -9,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello World");
     }
 }
