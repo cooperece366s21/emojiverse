dccb4a4811e5c6da119325f04c58b8626e921c04
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-03-06 19:14:21 -0500

    Add files via upload
    adding some react components

---
+++ EmojiVerse/src/main/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import {Login} from './components/login';
+
+function App() {
+  return (
+  <Router>
+    <div className="App">
+      <header className="App-header">
+        <Switch>
+			<Route exact path = "/" component = {Login} />
+		</Switch>
+      </header>
+    </div>
+   </Router>
+  );
+}
+
+export default App;
