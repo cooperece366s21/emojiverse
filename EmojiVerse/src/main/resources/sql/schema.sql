ee853cfa3eba5dda55a1d92a04b5b02768356d21
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-04-17 17:56:16 -0400

    added auto_increment and changed some primary keys

--- EmojiVerse/src/main/resources/sql/schema.sql
+++ EmojiVerse/src/main/resources/sql/schema.sql
@@ -2,28 +2,22 @@
 /* username : root */
 /* password : new_password */
 
-create table if not exists chat_participants(
-    chat_id int,
-    user_id int,
-    foreign key(user_id) references users(user_id),
-    foreign key(chat_id) references chat_list(chat_id),
-    primary key(user_id,chat_id)
-);
+/*Note: auto_increment automatically increments ids with unique numbers without having to insert using an insert statement*/
 
 create table if not exists chat_list(
-    chat_id int primary key not null,
-    chat_name varchar(50)                              
+    chat_id int primary key not null auto_increment,
+    chat_name varchar(50)
 );
 
 create table if not exists emoji_store(
-    emoji_id int primary key not null,
+    emoji_id int primary key not null auto_increment,
     emoji varchar(10),
     emoji_price int
 );
 
 
 create table if not exists users(
-    user_id int primary key not null,
+    user_id int primary key not null auto_increment,
     username varchar(50) not null unique,
     email varchar(50) not null unique,
     public_name varchar(50),
@@ -31,6 +25,14 @@ create table if not exists users(
     profile_img varchar(100), /* using aws s3 urls loaded thru boto*/
     permission_level int,
     emoji_coins int
+);
+
+create table if not exists chat_participants(
+    chat_id int,
+    user_id int,
+    foreign key(user_id) references users(user_id),
+    foreign key(chat_id) references chat_list(chat_id),
+    primary key(user_id,chat_id)
 );
 
 create table if not exists emojis(
@@ -43,7 +45,7 @@ create table if not exists emojis(
 );
 
 create table if not exists user_messages(
-    user_message_id int primary key not null,
+    user_message_id int primary key not null auto_increment,
     chat_id int,
     user_id int,
     foreign key(user_id) references users(user_id),
@@ -52,7 +54,8 @@ create table if not exists user_messages(
 );
 
 create table if not exists friends(
-    friend_username varchar(50) primary key not null,
+    friends_id int primary key not null auto_increment,
+    friend_username varchar(50) not null unique,
     friend_public_name varchar(50),
     user_id int,
     foreign key(user_id) references users(user_id),
@@ -60,7 +63,8 @@ create table if not exists friends(
 );
 
 create table if not exists blocked(
-    blocked_username varchar(50) primary key not null,
+    blocked_id int primary key not null auto_increment,
+    blocked_username varchar(50) not null unique,
     blocked_public_name varchar(50),
     blocked_profile_img varchar(100),
     user_id int,
