f8e1f6cbd3e2398eae08653e0228a1d5f4f6429b
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-04-18 21:49:21 -0400

    Update ChatDao.java

--- EmojiVerse/src/main/java/EmojiVerse/dao/ChatDao.java
+++ EmojiVerse/src/main/java/EmojiVerse/dao/ChatDao.java
@@ -7,9 +7,8 @@ import EmojiVerse.emoji.EmojiMessage;
 import EmojiVerse.user.User;
 
 public interface ChatDao {
-	Channel getChannelByID(String id);
-	Channel createChannel(List<User> users); //I assume that ID selection occurs automagically
-	void addMessage(Channel channel, EmojiMessage message);
-	void addUser(Channel channel, User requester, User target); //move authentication responsibility to DAO
-	void removeUser(Channel channel, User requester, User target);
+	String getChannelByID(int id);
+	void addMessage(Channel channel, String message, String username);
+	void addUser(Channel channel, String target_username); //move authentication responsibility to DAO
+	void removeUser(Channel channel, String target_username);
 }
