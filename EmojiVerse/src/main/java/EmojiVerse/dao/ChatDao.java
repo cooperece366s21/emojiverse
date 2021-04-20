dd3cc4c30c7f883dfbc0cdb24ade4d25c04e2729
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-04-20 13:01:27 -0400

    Update ChatDao.java

--- EmojiVerse/src/main/java/EmojiVerse/dao/ChatDao.java
+++ EmojiVerse/src/main/java/EmojiVerse/dao/ChatDao.java
@@ -11,4 +11,7 @@ public interface ChatDao {
 	void addMessage(Channel channel, String message, String username);
 	void addUser(Channel channel, String target_username); //move authentication responsibility to DAO
 	void removeUser(Channel channel, String target_username);
+	int getNextChatId();
+	String getMessages(String chat_name);
+	int getChatIdFromChatName(String chat_name);
 }
