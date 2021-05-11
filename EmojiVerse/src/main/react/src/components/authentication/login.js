00b6e5e8ca9e0ab1908d6edfb72a07684e557f9c
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-05-11 16:52:14 -0400

    Update login.js

--- EmojiVerse/src/main/react/src/components/authentication/login.js
+++ EmojiVerse/src/main/react/src/components/authentication/login.js
@@ -1,11 +1,4 @@
-import React, { useState} from 'react';
-import { Form, Input, Button } from 'semantic-ui-react';
-import 'semantic-ui-css/semantic.min.css';
-import {Redirect} from 'react-router-dom';
 
-export const Login = ()  => {
-  const [username, setUsername] = useState(''); //  Empty String
-  const [user_password,setPassword] = useState('')
   import React, { useState} from 'react';
 import { Form, Input, Button } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
