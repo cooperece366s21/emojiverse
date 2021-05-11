c50fad50ff7ae1d45c0301c5ab0300c6708f0ecf
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-05-11 16:32:33 -0400

    Create EmojiRowMapper.java

---
+++ EmojiVerse/src/main/java/EmojiVerse/emoji/EmojiRowMapper.java
@@ -0,0 +1,19 @@
+package EmojiVerse.emoji;
+
+import org.jdbi.v3.core.StatementContext;
+import org.jdbi.v3.core.mapper.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmojiRowMapper implements RowMapper<Emoji> {
+
+    @Override
+    public Emoji map(ResultSet rs, StatementContext ctx) throws SQLException {
+        Emoji emoji = new Emoji();
+        emoji.setName(rs.getString("emoji"));
+        emoji.setPrice(rs.getInt("emoji_price"));
+        emoji.setCategory(rs.getString("category"));
+        return emoji;
+    }
+}
