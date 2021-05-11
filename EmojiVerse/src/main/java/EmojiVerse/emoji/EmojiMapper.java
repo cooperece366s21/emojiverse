60e1480a649064d907e7b20496965e5932d2bb9d
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-05-11 16:33:14 -0400

    Update EmojiMapper.java

--- EmojiVerse/src/main/java/EmojiVerse/emoji/EmojiMapper.java
+++ EmojiVerse/src/main/java/EmojiVerse/emoji/EmojiMapper.java
@@ -22,8 +22,8 @@ public class EmojiMapper implements EmojiJDBI{
     @Override
     public void addEmojiCoins(String username) {
         jdbi.withHandle(h -> h.createUpdate("update users" +
-                "set emoji_coins = emoji_coins+1" +
-                "where username = :username")
+                " set emoji_coins = emoji_coins+1" +
+                " where username = :username")
                 .bind("username",username)
                 .execute());
 
@@ -41,14 +41,18 @@ public class EmojiMapper implements EmojiJDBI{
     }
 
     @Override
-    public int getEmojiCoins(String username) {
+    public String getEmojiCoins(String username) {
+        Gson gson = new Gson();
+        Map<String, Object> map = new HashMap<>();
         int emoji_coins = jdbi.withHandle(
                 handle ->
                         handle.createQuery("select emoji_coins from users where username  = :username")
                                 .bind("username",username)
-                                .map((rs, ctx) -> rs.getInt("user_id"))
+                                .map((rs, ctx) -> rs.getInt("emoji_coins"))
                                 .list().get(0));
-        return emoji_coins;
+        map.put("emoji_coins",emoji_coins);
+        System.out.println(emoji_coins);
+        return gson.toJson(map);
     }
 
     @Override
@@ -97,12 +101,31 @@ public class EmojiMapper implements EmojiJDBI{
     }
 
     @Override
-    public void populateEmojiStore(List<String> emojis, int price, String category)  {
-        System.out.println("hello");
-        for(int i =0; i < emojis.size(); i++)
+    public void populateEmojiStore(List<String> emojis, int price, String category) {
+
+
+        /*something fishy here. should not have zeros for majority of emojis*/
+        if (price == -1) {
+            for(int i =0; i < emojis.size(); i++)
+            {
+                price = (int) Math.ceil(Math.random()*3000);
+                if(price < 100)
+                {
+                    price = 0;
+                }
+                System.out.println(i);
+                System.out.println(price);
+                addEmojiToStore(emojis.get(i), price,category);
+            }
+
+        } else
         {
-            addEmojiToStore(emojis.get(i), price,category);
+            for(int i =0; i < emojis.size(); i++)
+            {
+                addEmojiToStore(emojis.get(i), price,category);
+            }
         }
+
 
     }
 
@@ -117,21 +140,112 @@ public class EmojiMapper implements EmojiJDBI{
     }
 
     @Override
-    public String getEmojisFromStore() {
+    public String getEmojisFromStore(String username) {
+        UserMapper userMapper = new UserMapper("jdbc:mysql://localhost:3306/emojiverse");
+        int user_id = userMapper.getUserIdFromUserName(username);
         System.out.println("hello");
         Gson gson = new Gson();
         Map<String, Object> map = new HashMap<>();
-        List<String> emoji_list = jdbi.withHandle(
+        Map<String, Object> fullMap = new HashMap<>();
+        List<Emoji> emoji_list = jdbi.withHandle(
                 handle ->
-                        handle.createQuery("select emoji from emoji_store")
-
-                                .map((rs, ctx) -> rs.getString("emoji"))
+                        handle.createQuery("select emoji,category,emoji_price from emoji_store" +
+                                " where emoji not in (select emoji from emoji_store" +
+                                " inner join emojis e on emoji_store.emoji_id = e.emoji_id" +
+                                " where user_id = :user_id) order by emoji_price")
+                                .bind("user_id",user_id)
+                                .map(new EmojiRowMapper())
                                 .list());
-        map.put("emoji_list",emoji_list);
+        List<String> PEOPLE_EMOJIS = new ArrayList<String>();
+        List<String> ANIMALS_NATURE_EMOJIS = new ArrayList<String>();
+        List<String> FOOD_SPORTS_EMOJIS = new ArrayList<String>();
+        List<String> TRAVEL_PLACES_EMOJIS = new ArrayList<String>();
+        List<String> OBJECTS_EMOJIS = new ArrayList<String>();
+        List<String> SYMBOLS_FLAGS_EMOJIS = new ArrayList<String>();
+
+        List<Integer> PEOPLE_EMOJIS_prices = new ArrayList<Integer>();
+        List<Integer> ANIMALS_NATURE_EMOJIS_prices = new ArrayList<Integer>();
+        List<Integer> FOOD_SPORTS_EMOJIS_prices = new ArrayList<Integer>();
+        List<Integer> TRAVEL_PLACES_EMOJIS_prices = new ArrayList<Integer>();
+        List<Integer> OBJECTS_EMOJIS_prices = new ArrayList<Integer>();
+        List<Integer> SYMBOLS_FLAGS_EMOJIS_prices = new ArrayList<Integer>();
+
+
+        List<Integer> prices = new ArrayList<Integer>();
+        for(Emoji emoji : emoji_list)
+        {
+
+            switch(emoji.getCategory())
+            {
+                case "PEOPLE_EMOJIS":
+                    PEOPLE_EMOJIS.add(emoji.getName() + " = "  + emoji.getPrice());
+                    PEOPLE_EMOJIS_prices.add(emoji.getPrice());
+                    break;
+                case "ANIMALS_NATURE_EMOJIS":
+                    ANIMALS_NATURE_EMOJIS.add(emoji.getName() + " = " + emoji.getPrice());
+                    ANIMALS_NATURE_EMOJIS_prices.add(emoji.getPrice());
+                    break;
+                case "FOOD_SPORTS_EMOJIS":
+                    FOOD_SPORTS_EMOJIS.add(emoji.getName() + " = " + emoji.getPrice());
+                    FOOD_SPORTS_EMOJIS_prices.add(emoji.getPrice());
+                    break;
+                case "OBJECTS_EMOJIS":
+                    OBJECTS_EMOJIS.add(emoji.getName() + " = " + emoji.getPrice());
+                    OBJECTS_EMOJIS_prices.add(emoji.getPrice());
+                    break;
+                case "TRAVEL_PLACES_EMOJIS":
+                    TRAVEL_PLACES_EMOJIS.add(emoji.getName() + " = " + emoji.getPrice());
+                    TRAVEL_PLACES_EMOJIS_prices.add(emoji.getPrice());
+                    break;
+                case "SYMBOLS_FLAGS_EMOJIS":
+                    SYMBOLS_FLAGS_EMOJIS.add(emoji.getName() + " = " + emoji.getPrice());
+                    SYMBOLS_FLAGS_EMOJIS_prices.add(emoji.getPrice());
+                    break;
+                default:
+                    System.out.println("Emoji does not exist");
+                    break;
+            }
+
+        }
+        map.put("PEOPLE_EMOJIS",PEOPLE_EMOJIS);
+        map.put("ANIMALS_NATURE_EMOJIS",ANIMALS_NATURE_EMOJIS);
+        map.put("FOOD_SPORTS_EMOJIS",FOOD_SPORTS_EMOJIS);
+        map.put("OBJECTS_EMOJIS", OBJECTS_EMOJIS);
+        map.put("SYMBOLS_FLAGS_EMOJIS",SYMBOLS_FLAGS_EMOJIS);
+        map.put("TRAVEL_PLACES_EMOJIS",TRAVEL_PLACES_EMOJIS);
+
+        map.put("PEOPLE_EMOJIS_prices",PEOPLE_EMOJIS_prices);
+        map.put("ANIMALS_NATURE_EMOJIS_prices",ANIMALS_NATURE_EMOJIS_prices);
+        map.put("FOOD_SPORTS_EMOJIS_prices",FOOD_SPORTS_EMOJIS_prices);
+        map.put("OBJECTS_EMOJIS_prices", OBJECTS_EMOJIS_prices);
+        map.put("SYMBOLS_FLAGS_EMOJIS_prices",SYMBOLS_FLAGS_EMOJIS_prices);
+        map.put("TRAVEL_PLACES_EMOJIS_prices",TRAVEL_PLACES_EMOJIS_prices);
+
+
+        System.out.println(PEOPLE_EMOJIS);
+        System.out.println(TRAVEL_PLACES_EMOJIS);
+
         return gson.toJson(map);
 
 
 
+    }
+
+    public String getEmojiPrice(String emoji)
+    {
+        Gson gson = new Gson();
+        Map<String, Object> map = new HashMap<>();
+
+        int price = jdbi.withHandle(
+                handle ->
+                        handle.createQuery("select emoji_price from emoji_store" +
+                                " where emoji = :emoji")
+                                .bind("emoji",emoji)
+                                .map((rs, ctx) -> rs.getInt("emoji_price"))
+                                .list().get(0));
+        map.put("price",price);
+        System.out.println(price);
+        return gson.toJson(map);
     }
 
     public String getUserEmojis(String username)
@@ -140,16 +254,69 @@ public class EmojiMapper implements EmojiJDBI{
         Map<String, Object> map = new HashMap<>();
         UserMapper userMapper  = new UserMapper("jdbc:mysql://localhost:3306/emojiverse");
         int user_id = userMapper.getUserIdFromUserName(username);
-        List<String> user_emoji_list = jdbi.withHandle(
+        List<Emoji> user_emoji_list = jdbi.withHandle(
                 handle ->
-                        handle.createQuery("select emoji from emoji_store " +
+                        handle.createQuery("select emoji,category,emoji_price from emoji_store " +
                                 "inner join emojis on emoji_store.emoji_id = emojis.emoji_id" +
-                                "where user_id = :user_id")
+                                " where user_id = :user_id")
                                 .bind("user_id",user_id)
-
-                                .map((rs, ctx) -> rs.getString("emoji"))
+                                .map(new EmojiRowMapper())
                                 .list());
-        map.put("user_emoji_list",user_emoji_list);
+        List<String> PEOPLE_EMOJIS = new ArrayList<String>();
+        List<String> ANIMALS_NATURE_EMOJIS = new ArrayList<String>();
+        List<String> FOOD_SPORTS_EMOJIS = new ArrayList<String>();
+        List<String> TRAVEL_PLACES_EMOJIS = new ArrayList<String>();
+        List<String> OBJECTS_EMOJIS = new ArrayList<String>();
+        List<String> SYMBOLS_FLAGS_EMOJIS = new ArrayList<String>();
+
+
+
+
+        List<Integer> prices = new ArrayList<Integer>();
+        for(Emoji emoji : user_emoji_list)
+        {
+
+            switch(emoji.getCategory())
+            {
+                case "PEOPLE_EMOJIS":
+                    PEOPLE_EMOJIS.add(emoji.getName());
+
+                    break;
+                case "ANIMALS_NATURE_EMOJIS":
+                    ANIMALS_NATURE_EMOJIS.add(emoji.getName());
+
+                    break;
+                case "FOOD_SPORTS_EMOJIS":
+                    FOOD_SPORTS_EMOJIS.add(emoji.getName());
+
+                    break;
+                case "OBJECTS_EMOJIS":
+                    OBJECTS_EMOJIS.add(emoji.getName());
+
+                    break;
+                case "TRAVEL_PLACES_EMOJIS":
+                    TRAVEL_PLACES_EMOJIS.add(emoji.getName());
+
+                    break;
+                case "SYMBOLS_FLAGS_EMOJIS":
+                    SYMBOLS_FLAGS_EMOJIS.add(emoji.getName());
+
+                    break;
+                default:
+                    System.out.println("Emoji does not exist");
+                    break;
+            }
+
+        }
+        map.put("PEOPLE_EMOJIS",PEOPLE_EMOJIS);
+        map.put("ANIMALS_NATURE_EMOJIS",ANIMALS_NATURE_EMOJIS);
+        map.put("FOOD_SPORTS_EMOJIS",FOOD_SPORTS_EMOJIS);
+        map.put("OBJECTS_EMOJIS", OBJECTS_EMOJIS);
+        map.put("SYMBOLS_FLAGS_EMOJIS",SYMBOLS_FLAGS_EMOJIS);
+        map.put("TRAVEL_PLACES_EMOJIS",TRAVEL_PLACES_EMOJIS);
+
+        System.out.println(TRAVEL_PLACES_EMOJIS);
+
         return gson.toJson(map);
     }
 
