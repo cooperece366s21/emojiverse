eedccaf322ef91874ff976698098dd65cd7f5b3c
Dan Brody <59486373+dbrody112@users.noreply.github.com>
2021-05-11 16:41:58 -0400

    simplified login.js

--- EmojiVerse/src/main/react/src/components/authentication/login.js
+++ EmojiVerse/src/main/react/src/components/authentication/login.js
@@ -6,6 +6,15 @@ import {Redirect} from 'react-router-dom';
 export const Login = ()  => {
   const [username, setUsername] = useState(''); //  Empty String
   const [user_password,setPassword] = useState('')
+  import React, { useState} from 'react';
+import { Form, Input, Button } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
+import {Redirect} from 'react-router-dom';
+import api from '../../services/api'
+
+export const Login = ()  => {
+  const [username, setUsername] = useState(''); //  Empty String
+  const [user_password,setPassword] = useState('')
   const [title,setTitle] = useState('')
   const[verified,setVerified] = useState(false)
   
@@ -31,80 +40,7 @@ export const Login = ()  => {
       <Form.Field>
 	  
         
-		<Button onClick = {async () => {
-		const user = username;
-		const new_password = user_password;
-		const get_response_chats = await fetch("/chats", {
-            method: "POST",
-            headers: {
-              "Content_Type": "application/json"
-            },
-            body:
-              JSON.stringify({
-			  username: user,user_password:new_password})
-            })
-			if (get_response_chats.ok) {
-            console.log("Response Worked! ");
-			get_response_chats.json().then(data=>{
-				console.log(data)
-				
-					
-				localStorage.setItem('chat_names', data.chat_names);
-				
-				});
-          }
-		  
-		  const get_response_friends = await fetch("/getFriend", {
-            method: "POST",
-            headers: {
-              "Content_Type": "application/json"
-            },
-            body:
-              JSON.stringify({
-			  username: user,user_password:new_password})
-            })
-			if (get_response_friends.ok) {
-            console.log("Response Worked! ");
-			get_response_friends.json().then(data=>{
-				console.log(data)
-				
-					
-				localStorage.setItem('friends', data.friends);
-				
-				});
-          }
-          
-          const post_response = await fetch("/login", {
-            method: "POST",
-            headers: {
-              "Content_Type": "application/json"
-            },
-            body:
-              JSON.stringify({
-			  username: user,user_password:new_password})
-            })
-		 
-		 
-		  
-          if (post_response.ok) {
-            console.log("Response Worked! ");
-			post_response.json().then(data=>{
-				setVerified(data.authorized)
-				console.log(data)
-				if(data.authorized===true)
-				{
-					
-					localStorage.setItem('access_token', data.access_token);
-					localStorage.setItem('username', data.username);
-					window.location.replace("http://localhost:3000/chatList")
-				}
-				});
-          }
-          else {
-            console.log("not found") 
-          }
-
-        }}> Sign In </Button>
+		<Button onClick = {async () => Authenticate(username, user_password)}> Sign In </Button>
 		
 		<a href="http://localhost:3000/signup">Sign Up</a>
       </Form.Field>
@@ -115,3 +51,10 @@ export const Login = ()  => {
     </Form>
   );
 };
+
+function Authenticate(username,user_password){
+    api.loadChatNames(username, user_password)
+	api.loadFriends(username, user_password)
+	api.authenticateCredentials(username, user_password)
+	api.loadEmojiCoins(username)
+}
